662286174b817fcdc67291d11aa8b3921bf09429
Laxmi <ranalxmi310@gmail.com>
2022-12-18 16:53:56 +0530

    formatting

--- src/com/demo/kkDSA/FunctionMethods.java
+++ src/com/demo/kkDSA/FunctionMethods.java
@@ -2,10 +2,10 @@ package com.demo.kkDSA;
 
 public class FunctionMethods {
     public static void main(String[] args) {
-        //pass by refrence and pass by value
+        //pass by reference and pass by value
         //java does not support pass by reference only pass by value
-        //when we pass primitive data to  the function it does not change the origin object value, eg. int char, byte, short
-        //but if we pass other data type like array and update the values the original object is changec.
+        //when we pass primitive data to  the function it does not change the origin object value, e.g. int char, byte, short
+        //but if we pass other data type like array and update the values the original object is changed.
         int a = 1;
         int b = 2;
         swap(a, b);
