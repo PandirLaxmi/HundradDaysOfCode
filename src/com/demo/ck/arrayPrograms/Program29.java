b9044142ddd567ab2c8f9e64a87f175103686baf
Laxmi <ranalxmi310@gmail.com>
2023-03-13 00:02:24 +0530

    updated column sum for an array

--- src/com/demo/ck/arrayPrograms/Program29.java
+++ src/com/demo/ck/arrayPrograms/Program29.java
@@ -3,21 +3,17 @@ package com.demo.ck.arrayPrograms;
 public class Program29 {
     public static void main(String[] args) {
         //Write a program to print sum of each column of 2D array
-        int[][] numbers =  {{1, 5}, //[1 5] [{i,j} {}]
-                            {2, 3}};//[2 3] [ ]
-
-        for (int i = 0; i < numbers.length; i++) { //column
+        int[][] numbers =  {{1, 5, 6}, //[1 5] [{i,j} {}]
+                            {2, 3, 7}};//[2 3] [ ]
+        int rows = numbers.length;
+        int columns = numbers[0].length;
+        for (int i = 0; i < columns; i++) { //column
             int columnSum = 0;
-            //int rowSum =0;
-            for (int j = 0; j < numbers[i].length; j++) {  //rows
-                /*System.out.print(numbers[i][j]);
-                System.out.print(" ");
-                rowSum += numbers[i][j];*/
+            for (int j = 0; j < rows; j++) {  //rows
                 columnSum += numbers[j][i];
             }
 
             System.out.println(columnSum);
-            //System.out.println(rowSum);
         }
 
     }
